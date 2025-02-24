0dce6a489ce4e61fac89603e9ddae5bda2dcd94c
Sérgio Viroli <serginhovirolli@gmail.com>
2025-02-24 11:54:58 -0300

    primeiro commit

---
+++ src/test/java/dev/javax10/CadastrosDeNinjas/CadastrosDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.javax10.CadastrosDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastrosDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
