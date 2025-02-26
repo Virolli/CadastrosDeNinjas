c757ea72d888c9104f98ea226604b0542c5f7e77
Sergio Viroli <serginhovirolli@gmail.com>
2025-02-26 10:43:18 -0300

    ria uma entidade no banco de dados apartir do NinjaModel [Aula3]

---
+++ src/main/java/dev/javax10/CadastrosDeNinjas/NinjaModel.java
@@ -0,0 +1,57 @@
+package dev.javax10.CadastrosDeNinjas;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastroNinja")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(Long id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
