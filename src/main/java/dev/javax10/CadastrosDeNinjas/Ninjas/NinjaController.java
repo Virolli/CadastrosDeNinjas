58c0b11be5a6ccc5d5516180784103d143901994
Sergio Viroli <serginhovirolli@gmail.com>
2025-02-28 14:59:17 -0300

    Fix: altera a rota para listar os nomes dos ninjas e injetar a dependencia do SERVICES

--- src/main/java/dev/javax10/CadastrosDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/javax10/CadastrosDeNinjas/Ninjas/NinjaController.java
@@ -1,16 +1,51 @@
 package dev.javax10.CadastrosDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.PublicKey;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    @Autowired
+    private NinjaService ninjaService;
 
     @GetMapping("/boasvindas")
     public String boasvindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+    //Adicionar ninja - Criar
+    @PostMapping("/adicionar")
+    public String adicionarNinja(){
+        return "Ninja Criado";
+    }
+
+    //Mostrar todos os Ninja - Read
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
+    }
+
+    //Mostrar Ninja por ID - Read
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasID(){
+        return "Mostrar todos os Ninjas por ID";
+    }
+
+    //Alterar dados do Ninja - Update
+    @PutMapping("/alterar")
+    public String alterarNinjaPorID(){
+        return "Alterar Ninja por ID";
+    }
+
+    //Deletar ninja - Delete
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorID(){
+        return "Deletar ninja por ID";
     }
 
 }
--- src/main/java/dev/javax10/CadastrosDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/dev/javax10/CadastrosDeNinjas/Ninjas/NinjaRepository.java
@@ -1,7 +1,9 @@
 package dev.javax10.CadastrosDeNinjas.Ninjas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
 
 
